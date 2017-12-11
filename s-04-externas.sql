9a2cf4b0b3dd7152760a1663c41dbd7d2be00fd1
netosaurio11 <neto1014@gmail.com>
2017-12-11 13:50:03 -0600

    Se crea script s-04-externas que ejemplifica el caso de la tabla externa empresa_paquetera_ext para contemplar aquellas empresas que aún no esten registradas

---
+++ s-04-externas.sql
@@ -0,0 +1,25 @@
+--@Autor(es): Mejia Valdiviezo Ernesto Daniel, Godoy Juarez Carlos
+--@Fecha de creación: 11/12/2017
+--@Descripción: Creación de tablas externas.
+create table empresa_paquetera_ext(
+	empresa_paquetera_id number(10,0),
+	nombre varchar2(40),
+	fecha_registro date,
+	zona char(1)
+);
+organization external (
+	type oracle_loader
+	default directory tmp_dir
+		access parameters (
+		records delimited by newline
+		badfile tmp_dir:'empresa_paquetera_ext_bad.log'
+		logfile tmp_dir:'empresa_paquetera_ext.log'
+		fields terminated by ','
+		missing field values are null
+		(
+			empresa_paquetera_id, nombre,fecha_registro date mask "dd/mm/yyyy",zona
+		)
+	)
+	location ('empresa_paquetera_ext.txt')
+)
+reject limit unlimited;
