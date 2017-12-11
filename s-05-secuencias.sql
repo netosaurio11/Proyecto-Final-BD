f9f78b2ca58ee3071f544cfc3ac2aac61eab19db
netosaurio11 <neto1014@gmail.com>
2017-12-11 14:10:39 -0600

    Se crea script s-05-secuencias

---
+++ s-05-secuencias.sql
@@ -0,0 +1,51 @@
+--@Autor(es): Mejia Valdiviezo Ernesto Daniel, Godoy Juarez Carlos
+--@Fecha de creación: 11/12/2017
+--@Descripción: Creación de secuencias necesarias para poder insertar registros.
+create sequence cliente_seq
+	start with 1
+	increment by 1
+	nomaxvalue
+	nominvalue
+	nocycle;
+create sequence direccion_seq
+	start with 1
+	increment by 1
+	nomaxvalue
+	nominvalue
+	nocycle;
+create sequence forma_pago_seq
+	start with 1
+	increment by 1
+	nomaxvalue
+	nominvalue
+	nocycle;
+create sequence producto_seq
+	start with 1
+	increment by 1
+	nomaxvalue
+	nominvalue
+	nocycle;
+create sequence precio_producto_seq
+	start with 1
+	increment by 1
+	nomaxvalue
+	nominvalue
+	nocycle;
+create sequence orden_compra
+	start with 1
+	increment by 1
+	nomaxvalue
+	nominvalue
+	nocycle;
+create sequence status_orden_seq
+	start with 1
+	increment by 1
+	nomaxvalue
+	nominvalue
+	nocycle;
+create sequence status_orden_historico_seq
+	start with 1
+	increment by 1
+	nomaxvalue
+	nominvalue
+	nocycle;
