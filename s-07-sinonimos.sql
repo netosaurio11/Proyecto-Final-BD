615aed2a6d9617bc3e99c56647299ced37ca580e
netosaurio11 <neto1014@gmail.com>
2017-12-11 16:17:06 -0600

    Se corrige script 6 y se agrega script s-07-sinonimos

---
+++ s-07-sinonimos.sql
@@ -0,0 +1,27 @@
+--@Autor(es): Mejia Valdiviezo Ernesto Daniel, Godoy Juarez Carlos
+--@Fecha de creación: 11/12/2017
+--@Descripción: Creación de sinonimos
+
+create or replace public synonym forma_pago_sinonimo
+for forma_pago;
+
+create or replace public synonym paquete_sinonimo
+for paquete;
+
+create or replace public synonym orden_compra_sinonimo
+for orden_compra;
+
+grant select on producto to sr_proy_invitado;
+grant select on factura to sr_proy_invitado;
+grant select on cliente to sr_proy_invitado;
+
+connect gm_proy_invitado / invitado
+
+CREATE SYNONYM gm_proy_admin.producto_sinonimo
+for producto;
+
+CREATE SYNONYM gm_proy_admin.factura_sinonimo
+for factura;
+
+CREATE SYNONYM gm_proy_admin.cliente_sinonimo
+for cliente;
